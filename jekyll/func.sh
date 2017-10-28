3a92c42f86fab3c272467a7bf3b923d8f2b4bad3
Ankit R Gadiya <git@argp.in>
2017-10-28 20:11:42 +0530

    Added Helped function for Jekyll Image

---
+++ jekyll/func.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# Helper Function for Jekyll Docker Image.
+# Place this file in appropriate location and then
+# source it in .bashrc to enable surge command.
+
+surge ()
+{
+	docker run \
+		--name jekyll \
+		--rm \
+		-it \
+		-v `pwd`:/source \
+		-p 4000:4000 \
+		jekyll:latest \
+		"$@"
+}
